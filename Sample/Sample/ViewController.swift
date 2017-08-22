5e33d622dee21e3e54b6034aa8a749dcfa1c4da7
Arror <hallo.maqiang@gmail.com>
2017-08-22 21:36:05 +0800

    Fix sample

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -21,8 +21,7 @@ class ViewController: UIViewController {
         
         var str2 = str1
         
-        str2.add(attributes: .foregroundColor(.green), range: str2.startIndex..<str2.endIndex)
-        str2.remove(attribute: .font, range: str2.startIndex..<str2.endIndex)
+        str2.add(attributes: .foregroundColor(.green), .font(.systemFont(ofSize: 30.0)), range: str2.startIndex..<str2.endIndex)
         
         self.label1.attributedText = str1 as NSAttributedString
         self.label2.attributedText = str2 as NSAttributedString
