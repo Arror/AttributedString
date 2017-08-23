a2497bfdf125200e5a4b0fbeef4bddce06f62797
Arror <hallo.maqiang@gmail.com>
2017-08-23 08:12:57 +0800

    Fix api style

--- AttributedString/Attribute.swift
+++ AttributedString/Attribute.swift
@@ -36,48 +36,27 @@ public enum Attribute {
         
         var nsKey: NSAttributedStringKey {
             switch self {
-            case .font:
-                return .font
-            case .paragraphStyle:
-                return .paragraphStyle
-            case .foregroundColor:
-                return .foregroundColor
-            case .backgroundColor:
-                return .backgroundColor
-            case .ligature:
-                return .ligature
-            case .kern:
-                return .kern
-            case .strikethroughStyle:
-                return .strikethroughStyle
-            case .underlineStyle:
-                return .underlineStyle
-            case .strokeColor:
-                return .strokeColor
-            case .strokeWidth:
-                return .strokeWidth
-            case .shadow:
-                return .shadow
-            case .textEffect:
-                return .textEffect
-            case .attachment:
-                return .attachment
-            case .link:
-                return .link
-            case .baselineOffset:
-                return .baselineOffset
-            case .underlineColor:
-                return .underlineColor
-            case .strikethroughColor:
-                return .strikethroughColor
-            case .obliqueness:
-                return .obliqueness
-            case .expansion:
-                return .expansion
-//            case .writingDirection:
-//                return .writingDirection
-//            case .verticalGlyphForm:
-//                return .verticalGlyphForm
+            case .font:                 return .font
+            case .paragraphStyle:       return .paragraphStyle
+            case .foregroundColor:      return .foregroundColor
+            case .backgroundColor:      return .backgroundColor
+            case .ligature:             return .ligature
+            case .kern:                 return .kern
+            case .strikethroughStyle:   return .strikethroughStyle
+            case .underlineStyle:       return .underlineStyle
+            case .strokeColor:          return .strokeColor
+            case .strokeWidth:          return .strokeWidth
+            case .shadow:               return .shadow
+            case .textEffect:           return .textEffect
+            case .attachment:           return .attachment
+            case .link:                 return .link
+            case .baselineOffset:       return .baselineOffset
+            case .underlineColor:       return .underlineColor
+            case .strikethroughColor:   return .strikethroughColor
+            case .obliqueness:          return .obliqueness
+            case .expansion:            return .expansion
+//            case .writingDirection:     return .writingDirection
+//            case .verticalGlyphForm:    return .verticalGlyphForm
             }
         }
     }
@@ -106,95 +85,53 @@ public enum Attribute {
     
     var key: Attribute.Key {
         switch self {
-        case .font:
-            return .font
-        case .paragraphStyle:
-            return .paragraphStyle
-        case .foregroundColor:
-            return .foregroundColor
-        case .backgroundColor:
-            return .backgroundColor
-        case .ligature:
-            return .ligature
-        case .kern:
-            return .kern
-        case .strikethroughStyle:
-            return .strikethroughStyle
-        case .underlineStyle:
-            return .underlineStyle
-        case .strokeColor:
-            return .strokeColor
-        case .strokeWidth:
-            return .strokeWidth
-        case .shadow:
-            return .shadow
-        case .textEffect:
-            return .textEffect
-        case .attachment:
-            return .attachment
-        case .link:
-            return .link
-        case .baselineOffset:
-            return .baselineOffset
-        case .underlineColor:
-            return .underlineColor
-        case .strikethroughColor:
-            return .strikethroughColor
-        case .obliqueness:
-            return .obliqueness
-        case .expansion:
-            return .expansion
-//        case .writingDirection:
-//            return .writingDirection
-//        case .verticalGlyphForm:
-//            return .verticalGlyphForm
+        case .font:                 return .font
+        case .paragraphStyle:       return .paragraphStyle
+        case .foregroundColor:      return .foregroundColor
+        case .backgroundColor:      return .backgroundColor
+        case .ligature:             return .ligature
+        case .kern:                 return .kern
+        case .strikethroughStyle:   return .strikethroughStyle
+        case .underlineStyle:       return .underlineStyle
+        case .strokeColor:          return .strokeColor
+        case .strokeWidth:          return .strokeWidth
+        case .shadow:               return .shadow
+        case .textEffect:           return .textEffect
+        case .attachment:           return .attachment
+        case .link:                 return .link
+        case .baselineOffset:       return .baselineOffset
+        case .underlineColor:       return .underlineColor
+        case .strikethroughColor:   return .strikethroughColor
+        case .obliqueness:          return .obliqueness
+        case .expansion:            return .expansion
+//        case .writingDirection:     return .writingDirection
+//        case .verticalGlyphForm:    return .verticalGlyphForm
         }
     }
     
     var value: Any {
         switch self {
-        case .font(let v):
-            return v
-        case .paragraphStyle(let v):
-            return v
-        case .foregroundColor(let v):
-            return v
-        case .backgroundColor(let v):
-            return v
-        case .ligature(let v):
-            return v
-        case .kern(let v):
-            return v
-        case .strikethroughStyle(let v):
-            return v
-        case .underlineStyle(let v):
-            return v
-        case .strokeColor(let v):
-            return v
-        case .strokeWidth(let v):
-            return v
-        case .shadow(let v):
-            return v
-        case .textEffect(let v):
-            return v
-        case .attachment(let v):
-            return v
-        case .link(let v):
-            return v
-        case .baselineOffset(let v):
-            return v
-        case .underlineColor(let v):
-            return v
-        case .strikethroughColor(let v):
-            return v
-        case .obliqueness(let v):
-            return v
-        case .expansion(let v):
-            return v
-//        case .writingDirection(let v):
-//            return v
-//        case .verticalGlyphForm(let v):
-//            return v
+        case .font(let v):                  return v
+        case .paragraphStyle(let v):        return v
+        case .foregroundColor(let v):       return v
+        case .backgroundColor(let v):       return v
+        case .ligature(let v):              return v
+        case .kern(let v):                  return v
+        case .strikethroughStyle(let v):    return v
+        case .underlineStyle(let v):        return v
+        case .strokeColor(let v):           return v
+        case .strokeWidth(let v):           return v
+        case .shadow(let v):                return v
+        case .textEffect(let v):            return v
+        case .attachment(let v):            return v
+        case .link(let v):                  return v
+        case .baselineOffset(let v):        return v
+        case .underlineColor(let v):        return v
+        case .strikethroughColor(let v):    return v
+        case .obliqueness(let v):           return v
+        case .expansion(let v):             return v
+//        case .writingDirection(let v):      return v
+//        case .verticalGlyphForm(let v):     return v
         }
     }
 }
