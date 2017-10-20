c7a93d366d9fac1d4977483acab76ce75c72c453
Arror <hallo.maqiang@gmail.com>
2017-10-20 10:44:25 +0800

    Update sample

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -19,12 +19,18 @@ class ViewController: UIViewController {
         
         let str1 = AttributedString(string: "Hallo world!", attributes: .foregroundColor(.red), .font(.systemFont(ofSize: 24.0)))
         
-        var str2 = str1
-        
-        str2.add(attributes: .foregroundColor(.green), .font(.systemFont(ofSize: 30.0)), range: str2.startIndex..<str2.endIndex)
-        
         self.label1.attributedTextEx = str1
+        
+        var str2 = AttributedString(string: "My email is hello@danielemargutti.com and my website is http://www.danielemargutti.com")
+        
+        str2.add(attributes: .font(.systemFont(ofSize: 14.0)), range: str2.fullRange)
+        
+        let mailRegular = Regular(pattern: "([A-Za-z0-9_\\-\\.\\+])+\\@([A-Za-z0-9_\\-\\.])+\\.([A-Za-z]+)", options: .caseInsensitive, matchingOptions: [])
+        let urlRegular = Regular(pattern: "http?://([-\\w\\.]+)+(:\\d+)?(/([\\w/_\\.]*(\\?\\S+)?)?)?", options: .caseInsensitive, matchingOptions: [])
+        
+        try! str2.add(attributes: .font(.systemFont(ofSize: 18.0)), .foregroundColor(.red), range: str2.fullRange, regular: mailRegular)
+        try! str2.add(attributes: .font(.systemFont(ofSize: 18.0)), .foregroundColor(.green), range: str2.fullRange, regular: urlRegular)
+        
         self.label2.attributedTextEx = str2
     }
 }
-
